29a65bf19ff652cde64d12c64ef72f5740b0f1cd
Dario <gmd409@gmail.com>
2016-10-23 17:57:15 +0200

    Add React, webpack, and set the project properly

---
+++ scripts/app.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+interface MainContainerState {}
+interface MainContainerProps {}
+
+class MainContainer extends React.Component<MainContainerProps, MainContainerState> {
+    render() {
+        return <h1>Hello holaa</h1>;
+    }
+}
+ReactDom.render(<MainContainer />, document.getElementById('content'));
